5664324d76ab0fca7f33a6f05872e203ecbf8afc
Lucas Lopes <lucaslopesr@outlook.com>
2019-12-23 23:10:27 -0300

    subindo material de estudos

---
+++ src/app/demos/arquitetura-componentes/produto.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
+
+import { ProdutoDashboardComponent } from './produto-dashboard/produto-dashboard.component';
+import { ProdutoDetalheComponent } from './componentes/produto-card-detalhe.component';
+
+import { ProdutoRoutingModule } from './produto.route';
+import { ProdutoCountComponent } from './componentes/produto-count.component';
+
+@NgModule({
+    declarations: [
+        ProdutoDashboardComponent,
+        ProdutoDetalheComponent,
+        ProdutoCountComponent
+    ],
+    imports: [
+        CommonModule,
+        ProdutoRoutingModule
+    ],
+    exports: []
+})
+export class ProdutoModule { }
